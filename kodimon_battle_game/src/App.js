1f160704da857411b37d4525dfee8f840f48b0fd
Patrik <pzelen00@fesb.hr>
2022-12-02 11:47:32 +0100

    initial commmit

---
+++ kodimon_battle_game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import StartPage from './components/StartPage'
+
+const App = () => {
+  return (
+    <>
+        <StartPage />
+    </>
+    
+  )
+}
+
+export default App
---
+++ kodimon_battle_game/src/components/Button/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ButtonComponent = styled.button`
+  color: #fff;
+  background-color: #0073BC;
+  border-radius: 35px;
+  border: 4px solid #73B9E5;
+  font-family: Bellota;
+  width: 200px;
+  height: 50px;
+  cursor: pointer;
+`
+
+const Button = ({children, type, onClick}) => {
+  return (
+    <ButtonComponent onClick={onClick} type={type}>
+        {children}
+    </ButtonComponent>
+  )
+}
+
+export default Button
