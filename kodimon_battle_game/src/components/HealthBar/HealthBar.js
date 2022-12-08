c28bc38ea3f928cbbbcc2065c65d38b48241d460
Patrik <pzelen00@fesb.hr>
2022-12-08 17:07:19 +0100

    commit

--- kodimon_battle_game/src/components/HealthBar/HealthBar.js
+++ kodimon_battle_game/src/components/HealthBar/HealthBar.js
@@ -5,6 +5,8 @@ const HealthBar = ({initialHP, newHP}) => {
 
     const [percentage, setPercentage] = useState();
     const [newwidth, setWidth] = useState();
+    const [color, setColor] = useState("");
+    const [borderColor, setBorderColor] = useState("");
 
     useEffect(() =>{
         percentageHealth(initialHP, newHP);
@@ -14,12 +16,25 @@ const HealthBar = ({initialHP, newHP}) => {
         const initialhp = initialHP;
         const newhp = newHP;
         let percentageHP = Math.round(newhp/initialhp * 100);
+
+        if(percentageHP >= 50){
+            setColor("#62FF84");
+            setBorderColor("#079325");
+        }else if(percentageHP < 50 && percentageHP > 30){
+            setColor("#FFCC00");
+            setBorderColor("#FFA500")
+        }else{
+            setColor("#FF7575");
+            setBorderColor("#FF0000");
+        }
+
+
         setPercentage(percentageHP);
-        setWidth(130 + percentageHP);
+        setWidth(percentageHP/100 * 230);
     }
 
   return (
-    <HealthBarComponent>
+    <HealthBarComponent WID={newwidth} colorMain={color} colorBorder={borderColor}>
         <h3>{percentage} %</h3>
         <div className='progress'>
             <div className='progress-done'>
@@ -43,18 +58,18 @@ h3{
     margin-bottom: 2px;
 }
 .progress {
-    background-color: grey;
-    border: 3px solid black;
+    background-color: #fff;
+    border: 3px solid ${(props) =>props.colorBorder};
     border-radius: 20px;
     height: 12px;
     width: 230px;
 }
 
 .progress-done {
-    background-color: yellow;
+    background-color: ${(props) => props.colorMain};
     border-radius: 20px;
     height: 100%;
-    width: 230px;
+    width: ${(props) => props.WID}px;
 }
 
 `
