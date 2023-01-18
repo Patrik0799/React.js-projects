c08f135875d27309a9c2ef03e94eea7f00f398a8
Patrik Zelenika <95618459+Patrik0799@users.noreply.github.com>
2023-01-18 15:36:24 +0100

    Delete kodimon_battle_game directory

--- kodimon_battle_game/src/reducers/pokemonReducer2.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-    switch(action.type){
-        case "FETCH_POKEMON_2":
-            return action.payload;
-        default:
-            return state;
-    }
-}
--- kodimon_battle_game/src/reducers/startPokemonHP.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-    switch(action.type){
-        case "INITIAL_SET_LEFT_HP":
-            return action.payload
-        default:
-            return state
-    }
-}
